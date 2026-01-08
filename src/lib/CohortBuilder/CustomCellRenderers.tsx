e7810068a075ce8a65d15aea99110662699e09bc
craigrbarnes <craig.barnes@intelligentlight.com>
2026-01-08 13:53:00 -0600

    update with export dataset to cohort

--- src/lib/CohortBuilder/CustomCellRenderers.tsx
+++ src/lib/CohortBuilder/CustomCellRenderers.tsx
@@ -19,59 +19,6 @@ const RenderDicomLink = ({ cell }: CellRendererFunctionProps) => {
     );
 };
 
-const JoinFields = (
-  { cell, row }: CellRendererFunctionProps,
-  ...args: Array<Record<string, unknown>>
-) => {
-  if (!cell?.getValue() || cell?.getValue() === '') {
-    return <span></span>;
-  } else {
-    if (
-      typeof args[0] === 'object' &&
-      Object.keys(args[0]).includes('otherFields')
-    ) {
-      const otherFields = args[0].otherFields as Array<string>;
-      const labels = otherFields.map((field) => {
-        return row.getValue(field);
-      });
-      return <Text fw={600}> {labels.join(' ')}</Text>;
-    }
-  }
-  return <span>Not configured</span>;
-};
-
-const RenderLinkCell = ({ cell }: CellRendererFunctionProps) => {
-  return (
-    <a href={`${cell.getValue()}`} target="_blank" rel="noreferrer">
-      <Text c="blue" td="underline" fw={700}>
-        {' '}
-        {cell.getValue() as ReactNode}{' '}
-      </Text>
-    </a>
-  );
-};
-
-const JoinFields = (
-  { cell, row }: CellRendererFunctionProps,
-  ...args: Array<Record<string, unknown>>
-) => {
-  if (!cell?.getValue() || cell?.getValue() === '') {
-    return <span></span>;
-  } else {
-    if (
-      typeof args[0] === 'object' &&
-      Object.keys(args[0]).includes('otherFields')
-    ) {
-      const otherFields = args[0].otherFields as Array<string>;
-      const labels = otherFields.map((field) => {
-        return row.getValue(field);
-      });
-      return <Text fw={600}> {labels.join(' ')}</Text>;
-    }
-  }
-  return <span>Not configured</span>;
-};
-
 const RenderLinkCell = ({ cell }: CellRendererFunctionProps) => {
   return (
     <a href={`${cell.getValue()}`} target="_blank" rel="noreferrer">
@@ -140,11 +87,6 @@ export const registerCohortTableCustomCellRenderers = () => {
     'link',
     'DicomLink',
     RenderDicomLink,
-  );
-  ExplorerTableCellRendererFactory().registerRenderer(
-    'string',
-    'JoinFields',
-    JoinFields,
   );
   ExplorerTableCellRendererFactory().registerRenderer(
     'link',

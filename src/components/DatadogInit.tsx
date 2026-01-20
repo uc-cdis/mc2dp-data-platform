d8c2a762db30a5379658ad3b53ace79216241918
Shawn O'Connor <ocshawn@ocshawn.com>
2026-01-20 11:49:56 -0600

    data dog code cleanup (#165)
    Co-authored-by: Shawn O'Connor <shawnoconnor@uchicago.edu>

--- src/components/DatadogInit.tsx
+++ src/components/DatadogInit.tsx
@@ -1,11 +1,10 @@
 import { datadogRum } from "@datadog/browser-rum";
 
-const ENV = process.env.NODE_ENV;
 const DATACOMMONS = process.env.NEXT_PUBLIC_DATACOMMONS || "gen3.2_generic_datacommons";
 
 datadogRum.init({
-  applicationId: '4ff9aad3-e5ca-477e-bf07-b0e2508500a8',
-  clientToken: 'pub096755ba850be1d26c801f30442873ee',
+  applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID || '',
+  clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN || '',
   site: 'ddog-gov.com',
   service: 'frontend-framework',
   env: `${DATACOMMONS}`,
